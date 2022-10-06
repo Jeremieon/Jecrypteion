f628be764898c4139927c2ce011b209ae713a3f9
Jeremieon <jerry_slade@outlook.com>
2022-10-06 15:06:28 +0200

    loop done

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #the main class aka the brain box
+from tabnanny import check
+
+
 class Machine:
     def __init__(self):
         pass
 
 crypo = Machine()
 
+if crypo.mode == 'e':
+    print(check.encrypt())
+else:
+    print(check.decrypt())
