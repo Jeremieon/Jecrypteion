e270ac38a95378b0af8a8faddd27131662b2fc76
Jeremieon <jerry_slade@outlook.com>
2022-10-06 14:39:50 +0200

    trying to add the exryption

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 class Machine:
     def __init__(self):
         pass
-
+    
+    def encrypt(self):
+        new_message = []
+        key_zipper = (self.keys,self.values)
+        dict_encrypt =dict(key_zipper)
+        for letter in dict_encrypt 
 crypo = Machine()
 
