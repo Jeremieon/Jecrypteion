fc54181a0bf177e5b785bfdc2619470486db0223
Jeremieon <jerry_slade@outlook.com>
2022-10-06 14:06:19 +0200

    Initialized the properties

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #the main class aka the brain box
 class Machine:
-    def __init__(self,msg,mode,keys,val):
-        pass
+    def __init__(self,msg,mode,keys,values):
+        self.msg = msg
+        self.mode = mode
+        self.keys = keys
+        self.values = values   
 
 message = input("Enter your secret : ")
 mode = input("Select crypto mode: encode(e) OR decrypt as default:")
 keywords = "ab7cd1ef8gh2ij9kl3mn0op4qr!st5uvwx6yz@#"
 values = keywords[-1] + keywords[0:-1]
 crypo = Machine(message,mode,keywords,values)
+
 
