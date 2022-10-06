57c4f12075bec757a05d38b8394e7a582776c20f
Jeremieon <jerry_slade@outlook.com>
2022-10-06 14:55:21 +0200

    decrypt done

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 class Machine:
     def __init__(self):
         pass
-
+    
+    def decrypt(self):
+        key_zipper = zip(self.values,self.keys)
+        dict_decrypt =dict(key_zipper)
+        new_message = ''.join([dict_decrypt[letter] for letter in self.msg.lower().strip()])
+        return new_message
 crypo = Machine()
 
