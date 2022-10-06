6fc3247d9a7df9d4af7c82255cdb640943c7d702
Jeremieon <jerry_slade@outlook.com>
2022-10-06 14:16:20 +0200

    fix: added variables and objects

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #the main class aka the brain box
 class Machine:
-    def __init__(self):
+    def __init__(self,msg,mode,keys,val):
         pass
 
-crypo = Machine()
+message = input("Enter your secret : ")
+mode = input("Select crypto mode: encode(e) OR decrypt as default:")
+keywords = "ab7cd1ef8gh2ij9kl3mn0op4qr!st5uvwx6yz@#"
+values = keywords[-1] + keywords[0:-1]
+crypo = Machine(message,mode,keywords,values)
 
