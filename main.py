593fa4bf2d10555bf0b8638a758df3a1691d3982
Jeremieon <jerry_slade@outlook.com>
2022-10-06 14:59:02 +0200

    text update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-#the main class aka the brain box
+#the main class
+#Work in progress!!
 class Machine:
+    #initializing 
     def __init__(self,msg,mode,keys,val):
         pass
-
+#declaring object variables
 message = input("Enter your secret : ")
 mode = input("Select crypto mode: encode(e) OR decrypt as default:")
 keywords = "ab7cd1ef8gh2ij9kl3mn0op4qr!st5uvwx6yz@#"
 values = keywords[-1] + keywords[0:-1]
+#Main Object
 crypo = Machine(message,mode,keywords,values)
 
