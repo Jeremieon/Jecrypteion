460baecd6b5fc0c864573d082c4224d693d3f2a7
Jeremieon <jerry_slade@outlook.com>
2022-10-20 21:39:04 +0200

    removed all variables

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #the main class
 class Machine:
     #initializing 
-    def __init__(self,msg,mode,keys,val):
-        self.msg = msg
-        self.mode = mode
-        self.keys = keys
-        self.val = val
-    
+    def __init__(self):
+        self.msg = input("Enter your secret message: ")
+        self.mode = input("Select crypto mode: encode(e) OR decrypt as default:")
+        self.keys= "ab7cd1ef8gh2ij9kl3mn0op4qr!st5uvwx6yz@#"
+        self.val =  self.keys[-1] +  self.keys[0:-1]
+
     def encrypt(self):
         key_zipper = zip(self.keys,self.val)
         dict_encrypt =dict(key_zipper)
@@ -20,12 +20,12 @@ class Machine:
         new_message = ''.join([dict_decrypt[letter] for letter in self.msg.lower().strip()])
         return new_message
 #declaring object variables
-message = input("Enter your secret message: ")
-mode = input("Select crypto mode: encode(e) OR decrypt as default:")
-keywords = "ab7cd1ef8gh2ij9kl3mn0op4qr!st5uvwx6yz@#"
-values = keywords[-1] + keywords[0:-1]
+#message = input("Enter your secret message: ")
+#mode = input("Select crypto mode: encode(e) OR decrypt as default:")
+#keywords = "ab7cd1ef8gh2ij9kl3mn0op4qr!st5uvwx6yz@#"
+#values = keywords[-1] + keywords[0:-1]
 #Main Object
-crypo = Machine(message,mode,keywords,values)
+crypo = Machine()
 
 
 if __name__ == "__main__":
